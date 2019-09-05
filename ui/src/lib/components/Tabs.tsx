c7524db56f5c8d17c78674567eb5e01099cc2c07
jon borchardt <jon.borchardt@gmail.com>
2019-09-05 08:13:51 -0700

    pr fixes

--- ui/src/lib/components/Tabs.tsx
+++ ui/src/lib/components/Tabs.tsx
@@ -1,26 +1,27 @@
 import { Tabs as AntTabs } from 'antd';
 import styled, { StyledComponentBase } from 'styled-components';
 
-// exporting TabPane as a convenience
-// we could choose to not export, and just let users grab from Tabs below
-export const TabPane = styled(AntTabs.TabPane)``;
+// Styled version of TabPane.
+const TabPane = styled(AntTabs.TabPane)``;
 
 // Our exported styled component version of AntTabs will need to have a reference to the styled
-// component version of TabPane
-// there is a type swap here, this makes typescript happy (Antd.Tabs.TabPane => TabPane)
-// this is fine, since consumers will use the styled version and 'ref' grabs the inner component
+// component version of TabPane.
+// There is a type swap here, this makes typescript happy (Antd.Tabs.TabPane => TabPane).
+// This is fine, since consumers will use the styled version and 'ref' grabs the inner component.
 class VarnishTabs extends AntTabs {
     static TabPane = TabPane as any;
 }
 
-// AntTabs has static properties, but they are lost when converting to a styled component
-// this interface tells typescript about the styled component version of the statics
+// AntTabs has static properties, but they are lost when converting to a styled component.
+// This interface tells typescript about the styled component version of the statics.
 interface WithStaticProps extends StyledComponentBase<typeof AntTabs, any, {}, never> {
     TabPane: typeof TabPane;
 }
 
-// finally, we export our styled Tab, and tell typescript its a WithStaticProps
-export const Tabs = styled(VarnishTabs).attrs({ animated: false })`
+// Finally, we export our styled Tab, and tell typescript its a WithStaticProps.
+export const Tabs = styled(VarnishTabs).attrs((props) => ({
+    animated: false
+}))`
     &,
     .ant-tabs-nav-container {
         font-size: inherit;
