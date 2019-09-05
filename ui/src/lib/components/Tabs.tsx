20a9f2ac0ee8658e08c966557bba09611fb55353
jon borchardt <jon.borchardt@gmail.com>
2019-09-05 09:21:08 -0700

    pr fix

--- ui/src/lib/components/Tabs.tsx
+++ ui/src/lib/components/Tabs.tsx
@@ -2,24 +2,18 @@ import { Tabs as AntTabs } from 'antd';
 import styled, { StyledComponentBase } from 'styled-components';
 
 // Styled version of TabPane.
-const TabPane = styled(AntTabs.TabPane)``;
+const StyledTabPane = styled(AntTabs.TabPane)``;
 
 // Our exported styled component version of AntTabs will need to have a reference to the styled
 // component version of TabPane.
 // There is a type swap here, this makes typescript happy (Antd.Tabs.TabPane => TabPane).
 // This is fine, since consumers will use the styled version and 'ref' grabs the inner component.
 class VarnishTabs extends AntTabs {
-    static TabPane = TabPane as any;
+    static TabPane = StyledTabPane as any;
 }
 
-// AntTabs has static properties, but they are lost when converting to a styled component.
-// This interface tells typescript about the styled component version of the statics.
-interface WithStaticProps extends StyledComponentBase<typeof AntTabs, any, {}, never> {
-    TabPane: typeof TabPane;
-}
-
-// Finally, we export our styled Tab, and tell typescript its a WithStaticProps.
-export const Tabs = styled(VarnishTabs).attrs((props) => ({
+// Styled version of Tabs.
+const StyledTabs = styled(VarnishTabs).attrs((props) => ({
     animated: false
 }))`
     &,
@@ -30,4 +24,14 @@ export const Tabs = styled(VarnishTabs).attrs((props) => ({
     && .ant-tabs-tabpane {
         transition: none;
     }
-` as any as WithStaticProps;
+`;
+
+// AntTabs has static properties, but they are lost when converting to a styled component.
+// This interface tells typescript about the styled component version of the statics.
+interface WithStaticProps extends StyledComponentBase<typeof AntTabs, any, any, any> {
+    TabPane: typeof StyledTabPane;
+}
+
+// We trick Typescript here by first casting to unknown and then casting to the type we'd like to export.
+// We do this so that the exported type includes the static properties, which we lose otherwise.
+export const Tabs = StyledTabs as unknown as WithStaticProps;
